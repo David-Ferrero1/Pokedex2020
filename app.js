f11df4687e04276b4bcc4e402c9b72398b3804ab
David-Ferrero1 <ferrero_d@hotmail.com>
2020-10-05 18:39:29 +0200

    function fetchPokemonBase et Complet avec test console.log

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
+//On va récupérer les données brut pour les transformer en français.
+let allPokemon = [];
+let tableauFin = [];
+
 //On va selectionner notre input pour faire disparaitre le texte au moment de la recherche
 
 const searchInput = document.querySelector('.recherche-poke input');
 
+// On va appeler l'API de Pokemon
 function fetchPokemonBase(){
     fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
         .then(reponse => reponse.json())
         .then((allPoke) => {
-            console.log(allPoke);
+            //console.log(allPoke); Ca fonctionne, on en a plus besoin
+            allPoke.results.forEach((pokemon) => { // Pour chaque élément du tableau on envoi une fonction avec pokemon comme paramètre
+                fetchPokemonComplet(pokemon);
+            });
         });
 }
 fetchPokemonBase();
+
+function fetchPokemonComplet(pokemon) {
+
+    let objPokemonFull = {}; //Pokemon complet avec image, nom, type...
+    let url = pokemon.url;
+    let nameP = pokemon.name;
+
+    fetch(url)
+        .then(reponse => reponse.json())
+        .then((pokeData) => {
+            console.log(pokeData);
+        })
+}
 
 //Animation input, ie dès qu'on va entrer dans l'input
 
